92995ee42e0de0d419b4c214b6f0ec75edd7f097
Rodrigo Alvarez Garrote <70945401+rodralva@users.noreply.github.com>
2024-07-05 17:07:35 +0200

    calibration example for Jorge

--- lib/vis_functions.py
+++ lib/vis_functions.py
@@ -2,11 +2,11 @@
 import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
 
-def horizontal_subplots(x,ys,yer,legend):
+def horizontal_subplots(x,ys,yer,legend,wspace=0):
     colors=list(mcolors.TABLEAU_COLORS.keys())
     fig, axs = plt.subplots(dpi=200,ncols=len(legend),figsize=[10,3],sharey=True)
     plt.suptitle("Pedestal Noise level over Runs", fontsize=14)
-    fig.subplots_adjust(wspace=0)
+    fig.subplots_adjust(wspace=wspace)
 
     for i in range(len(legend)):
         axs[i].errorbar (x,ys[i], yerr=[yer[i],yer[i]],color= colors[i],linewidth=0,marker='o',markersize=1,elinewidth=1,capsize=2)
